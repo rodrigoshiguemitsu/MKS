8b6cce7c3dcafbfdbecece47a71605f2a1b65b8c
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2024-04-28 08:11:47 -0300

    ...

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 
 const apiLocal = axios.create({
-    baseURL:'https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1'
+    baseURL:'https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1',
+    headers:{
+        'Access-Control-Allow-Origin':'*',
+        'Content-Type':'application/json',
+    }
 })
 export default apiLocal
